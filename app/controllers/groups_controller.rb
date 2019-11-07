caad7e250a438d4246e2e42e872a0f4a6e17baea
shingomurakami59 <joyfut10shingo@gmail.com>
2019-11-07 22:01:07 +0900

    不要ファイルの削除

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -29,11 +29,11 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-  params.require(:group).permit(:name, user_ids: [] )
+    params.require(:group).permit(:name, user_ids: [] )
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
 end
-
-def set_group
-  @group = Group.find(params[:id])
-end
-
-
