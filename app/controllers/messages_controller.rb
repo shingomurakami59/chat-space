9a9313dd4fcde6f119bc9cf9f898c841b842ad30
shingomurakami59 <joyfut10shingo@gmail.com>
2019-11-13 14:59:04 +0900

    スペースの訂正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,38 +1,38 @@
 class MessagesController < ApplicationController
-	before_action :set_group
+  before_action :set_group
 
-	def index
-		@message = Message.new
-		@messages = @group.messages.includes(:user)
-		respond_to do |format|
-			format.html
-			format.json
-		end
-	end
+  def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
 
-	def create
-		@message = @group.messages.new(message_params)
+  def create
+    @message = @group.messages.new(message_params)
 
-		if @message.save
-			respond_to do |format|
-				format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'}
-				format.json
-	  	end
+    if @message.save
+      respond_to do |format|
+        format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'}
+        format.json
+      end
 
-		else
-		@messages = @group.messages.includes(:user)
-		flash.now[:alert] = "メッセージを入力してください。"
-		render :index
-    	end
-	end
+    else
+    @messages = @group.messages.includes(:user)
+    flash.now[:alert] = "メッセージを入力してください。"
+    render :index
+      end
+  end
 
 private
 
-	def message_params
-		params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
-	end
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 
-	def set_group
-		@group = Group.find(params[:group_id])
-	end
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
 end
