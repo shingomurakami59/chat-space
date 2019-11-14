4ad7976330de8369d447720c26096678219fca6b
shingomurakami59 <joyfut10shingo@gmail.com>
2019-11-14 16:01:13 +0900

    デプロイについての設定・ファイル編集

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+server '18.177.19.93', user: 'ec2-user', roles: %w{app db web}
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
