645641cd6cb5d446e5667039cb723ba824bfdd5f
shingomurakami59 <joyfut10shingo@gmail.com>
2019-11-16 08:25:35 +0900

    Update users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,16 +1,16 @@
 $(function() {
   function addUser(user) {
-    var html = `
+    let html = `
       <div class="chat-group-user clearfix">
         <p class="chat-group-user__name">${user.name}</p>
-        <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
       </div>
     `;
     $("#user-search-result").append(html);
   }
 
   function addNoUser() {
-    var html = `
+    let html = `
       <div class="chat-group-user clearfix">
         <p class="chat-group-user__name">ユーザーが見つかりません</p>
       </div>
@@ -18,19 +18,15 @@ $(function() {
     $("#user-search-result").append(html);
   }
   function addDeleteUser(name, id) {
-    var html = `
-    <div class="chat-group-user clearfix" id="${id}">
-      <p class="chat-group-user__name">${name}</p>
-      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+    let html = `
+    <div class="ChatMember clearfix" id="${id}">
+      <p class="ChatMember__name">${name}</p>
+      <div class="ChatMember__remove ChatMember__button" data-user-id="${id}" data-user-name="${name}">削除</div>
     </div>`;
-    $(".js-add-user").append(html);
-  }
-  function addMember(userId) {
-    var html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
-    $(`#${userId}`).append(html);
+    $(".ChatMembers").append(html);
   }
   $("#user-search-field").on("keyup", function() {
-    var input = $("#user-search-field").val();
+    let input = $("#user-search-field").val();
     $.ajax({
       type: "GET",
       url: "/users",
@@ -53,19 +49,5 @@ $(function() {
       .fail(function() {
         alert("通信エラーです。ユーザーが表示できません。");
       });
-  });
-  $(document).on("click", ".chat-group-user__btn--add", function() {
-    const userName = $(this).attr("data-user-name");
-    const userId = $(this).attr("data-user-id");
-    $(this)
-      .parent()
-      .remove();
-    addDeleteUser(userName, userId);
-    addMember(userId);
-  });
-  $(document).on("click", ".chat-group-user__btn--remove", function() {
-    $(this)
-      .parent()
-      .remove();
   });
 });
