ba0cd92a076cb2c799f277a9a4a1a7b84e347020
shingomurakami59 <joyfut10shingo@gmail.com>
2019-11-15 22:15:41 +0900

    test

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,7 +3,7 @@ $(function() {
     let html = `
       <div class="chat-group-user clearfix">
         <p class="chat-group-user__name">${user.name}</p>
-        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+        <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="2824" data-user-name="test1">追加</a>
       </div>
     `;
     $("#user-search-result").append(html);
